98945a52aba82e147ff089229102c5fd04d7d99a
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-25 16:44:22 +0200

    clock

---
+++ myscript.js
@@ -0,0 +1,25 @@
+const totalTimeInSecond = 10000;
+
+//*const totalTimeMinutes = 60;
+
+//*const totalTimeHours = 60
+
+//*const totalTimeDay = 60
+
+let secondToShow = 10;
+
+const countDown = setInterval(function(){
+    secondToShow--;
+    console.log(secondToShow);
+    
+}, 10000)
+
+
+
+//? tempo totale (20 * 60 * 60 * 1000) 
+
+
+setTimeout(function (){
+    alert('Inizia la lezione!!');
+    clearInterval(countDown);
+}, totalTimeInSecond);
